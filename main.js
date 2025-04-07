a3f478e7029032d0ee12b2efae6d013815539e6c
grantkendall <137111847+grantkendall@users.noreply.github.com>
2025-04-06 23:33:19 -0500

    Fix gallery bug
    Correct images populate the full-size slot and change accordingly when
    changing projects. Added more images and project names. Organized CSS.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Switching between projects
+// Linking project name with project content
 const projectList = document.querySelectorAll("li");
 const projects = document.querySelectorAll(".project");
 
@@ -10,23 +10,28 @@ projectList.forEach(name => {
             project.classList.remove("active");
         });
 
-        const targetProject = document.getElementById(targetName)
+        const targetProject = document.getElementById(targetName);
         if (targetProject) {
             targetProject.classList.add("active");
-        }
+        };
+
+        resetGallery();
     });
 });
 
-// Gallery logic
-// if -> project is 'active'...
-projects.forEach(project => {
-    const thumbs = document.querySelectorAll(".thumb");
+function resetGallery() {
+    const gallery = document.querySelector(".gallery");
     const fullImg = document.querySelector(".gallery-img");
-        
-    thumbs.forEach(img => {
-        img.addEventListener("click", () => {
-            console.log(img.src);
-            fullImg.src = img.src;
-        })
-    });
-});
+    const firstThumb = gallery.querySelector(".thumb");
+
+    if (firstThumb) {
+        fullImg.src = firstThumb.src;
+    };
+};
+
+document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("thumb")) {
+        const fullImg = event.target.closest(".gallery").querySelector(".gallery-img");
+        fullImg.src = event.target.src;
+    }
+})
